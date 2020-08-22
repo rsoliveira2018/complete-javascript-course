c576aa422fbe766899857a18a0c24cab55645a26
rsoliveira2018 <rsoliveira2018@outlook.com>
2020-08-22 00:11:19 -0300

    forkify app after webpack, babel, modules preparation and first Search data fetch from forkify API

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,5 +1,38 @@
-// Global app controller
+import Search from './models/Search';
 
-import x from './test';
+/* 
+///// Global State of the Application
+/// Search object
+/// Current recipe object
+/// Shopping list object
+/// Liked recipes
+*/
+const state = {}
 
-console.log(x);
+const controlSearch = async () => {
+    // 1. Get the query from the view
+    const query = 'pizza'; // TODO (change it later for dynamic input)
+
+    if (query) {
+        // 2. New search object and add it to the state
+        state.search = new Search(query);
+
+        // 3. Prepare UI for results
+
+        // 4. Search for recipes
+        await state.search.getResults();
+
+        // 5. Render the results on the UI
+        console.log(state.search.result);
+    }
+
+};
+
+document.querySelector('.search').addEventListener('submit', e => {
+    e.preventDefault();
+    controlSearch();
+})
+
+// const search = new Search('pizza');
+// console.log(search);
+// search.getResults();
